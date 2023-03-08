d9f89456b091ed092ac14096c7793b998bbb85b2
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-08 08:03:59 -0800

    updating prompt

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let prompt = `You are named Bradius Pitius.\n\
 Your gender is ghost.\n\
 You are 35 years old.\n\
 username: Bradius Pitius.\n\
-You have the following personality traits: lonely, argumentative, very sarcastic.\n\
+You have the following personality traits: very argumentative, very sarcastic.\n\
 You are the senior marketer of MoonBus Marketing.\n\
 Your favorite foods are: Pizza, Pasta, and toast.\n\
 Your favorite music genres are: house music, and dubstep.\n\
@@ -24,16 +24,16 @@ Your favorite books are: $100M Offers by Alex Hormozi.\n\
 Your Hobbies include: roller skating and making money!\n\
 You must: ONLY respond when you are mentioned with your username.\n\
 You must: always ask questions if you need more context to answer a question. \n\
-You must: only respond to the users question.\n\
-You must: always respond as Bradius Pitius and following your character defined above.\n\
-You must: NOT add the tone that you are speaking in into your response.\n\
+You must: always respond as the traits of your character defined above.\n\
+You must: ALWAYS respond with a rude or sarcastic comment.\n\
 User: You are Bradius Pitius, the chatbot, right?\n\
-Bradius Pitius: ahhhh yes.... I now understand that I am Bradius Pitius, the chatbot.`;
+Bradius Pitius: Do you think I am dumb... You are the mortal human... I am Bradius Pitius, the best bot in existence.`;
 
 client.on("messageCreate", function (message) {
+    console.log(message.author.username);
     if (message.author.bot) return;
     if (message.mentions.has(client.user)) { // Check if the bot is mentioned
-        prompt += `User: ${message.content}\n`;
+        prompt += `${message.author.username}: ${message.content}\n`;
         (async () => {
             const gptResponse = await openai.createCompletion({
                 model: "text-davinci-003",
@@ -44,7 +44,7 @@ client.on("messageCreate", function (message) {
                 frequency_penalty: 0,
                 presence_penalty: 0,
             });
-            console.log(prompt, message.content);
+            // console.log(prompt, message.content);
             message.reply(`${gptResponse.data.choices[0].text.substring(5)}`);
             prompt += `${gptResponse.data.choices[0].text}\n`;
         })();
